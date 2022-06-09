e5e8f420d45fc8108e9165bd57c15e7e9fe16524
ankit.yadav <ankit.yadav@ihsmarkit.com>
2022-06-09 12:20:21 +0530

    setup commit

---
+++ src/main/java/com/algo/trading/algotrading/AlgoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.algo.trading.algotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algo/trading/algotrading/AlgoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algo.trading.algotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
