941aa3b535c0d26f5699b48caeae8987df2ef077
ANKIT YADAV <any65889@gmail.com>
2023-01-11 13:03:14 +0530

    update working file

--- src/main/java/com/trading/AlgoTradingApplication.java
+++ src/main/java/com/trading/AlgoTradingApplication.java
@@ -11,44 +11,120 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgoTradingApplication {
 
-	@Value("${angel.username}")
-	private static String username;
+	 private static HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.COOKIE, "AWSALBAPP-0=_remove_; AWSALBAPP-1=_remove_; AWSALBAPP-2=_remove_; AWSALBAPP-3=_remove_; AWSALB=2W/GV5RHWOnI1eKt8uaZZGlniavOP19H51ajcdACjRFKUYg1pXtMdosTOJYBMUm5+t7gFPQCFoYTeyXiJ3BXMHmdNqTcb7RnnW+S6BJaaay0rQtaJbdMrb8ii5NN; AWSALBAPP-0=_remove_; AWSALBAPP-1=_remove_; AWSALBAPP-2=_remove_; AWSALBAPP-3=_remove_; AWSALBCORS=2W/GV5RHWOnI1eKt8uaZZGlniavOP19H51ajcdACjRFKUYg1pXtMdosTOJYBMUm5+t7gFPQCFoYTeyXiJ3BXMHmdNqTcb7RnnW+S6BJaaay0rQtaJbdMrb8ii5NN");
+        headers.add(HttpHeaders.ACCEPT_LANGUAGE, "en,gu;q=0.9,hi;q=0.8");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
 
-	@Value("${angel.password}")
-	private static String password;
+        return headers;
+    }
 
-	@Value("${angel.api.key}")
-	private static String apiKey;
+    public static void main(String[] args) throws Exception {
+        api();
+        //angel();
+    }
+
+    private static void angel() {
+        SmartConnect smartConnect = new SmartConnect();
+
+        // Provide your api key here
+        smartConnect.setApiKey("a5Dd7nxn");
+
+        // Set session expiry callback.
+        smartConnect.setSessionExpiryHook(new SessionExpiryHook() {
+            @Override
+            public void sessionExpired() {
+                System.out.println("session expired");
+            }
+        });
+
+        User user = smartConnect.generateSession("A844782", "9725", getTOTPCode("4TLPUQ4SFZKRXMEYSFRBKGKFOY"));
+
+        String feedToken = user.getFeedToken();
+        System.out.println(feedToken);
+
+        System.out.println(user.toString());
+        smartConnect.setAccessToken(user.getAccessToken());
+        smartConnect.setUserId(user.getUserId());
+//
+//        // token re-generate
+//
+//        TokenSet tokenSet = smartConnect.renewAccessToken(user.getAccessToken(),
+//                user.getRefreshToken());
+//        smartConnect.setAccessToken(tokenSet.getAccessToken());
+
+    }
+
+    public static String getTOTPCode(String secretKey) {
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(secretKey);
+        String hexKey = Hex.encodeHexString(bytes);
+        return TOTP.getOTP(hexKey);
+    }
 
 
-	public static void main(String[] args) {
+    private static void api() {
+        HttpEntity<String> request = new HttpEntity<>(getHeaders());
 
-		// Initialize Samart API using clientcode and password.
-		SmartConnect smartConnect = new SmartConnect();
+        String url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json";
 
-		// Provide your api key here
-		smartConnect.setApiKey(apiKey);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(url);
 
-		// Set session expiry callback.
-		smartConnect.setSessionExpiryHook(new SessionExpiryHook() {
-			@Override
-			public void sessionExpired() {
-				System.out.println("session expired");
-			}
-		});
 
-		User user = smartConnect.generateSession(username, password);
-		System.out.println(user.toString());
-		smartConnect.setAccessToken(user.getAccessToken());
-		smartConnect.setUserId(user.getUserId());
+        String urlTemplate = uriComponentsBuilder.uriVariables(new HashMap<>()).toUriString();
+        RestTemplate restTemplate = new RestTemplate();
 
-		// token re-generate
+        DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
+        defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
 
-		TokenSet tokenSet = smartConnect.renewAccessToken(user.getAccessToken(),
-				user.getRefreshToken());
-		smartConnect.setAccessToken(tokenSet.getAccessToken());
+        restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
+        restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 
-		SpringApplication.run(AlgoTradingApplication.class, args);
-	}
+        ResponseEntity apiResponse;
+        String resource = null;
+        try {
+            apiResponse = restTemplate.exchange(
+                    urlTemplate,
+                    HttpMethod.GET,
+                    request,
+                    String.class
+            );
+            resource = apiResponse.getBody().toString();
+           // System.out.println(resource);
+        } catch (Exception e) {
+            System.out.println("EXCEPTION WHILE INVOKING API KEY REQUEST : " + e);
+        }
 
+        JsonArray angelIndexArray = resource == null ? null :
+                StringUtils.isBlank(resource) ?
+                        null : new Gson().fromJson(resource, JsonArray.class);
+
+        JsonArray niftyArray = new JsonArray();
+        for (JsonElement angelElement : angelIndexArray) {
+            if (getSingleData(angelElement, "name").equalsIgnoreCase("NIFTY")
+                    && (getSingleData(angelElement, "instrumenttype").equalsIgnoreCase("OPTIDX"))) {
+                niftyArray.add(angelElement);
+            }
+        }
+
+        System.out.println(niftyArray.size());
+        System.out.println("=============================================");
+
+        for(JsonElement ele : niftyArray){
+          //  System.out.println(getSingleData(ele,"symbol"));
+        }
+
+    }
+
+    private static String getSingleData(JsonElement angelElement, String byName) {
+        return initializeParams(((JsonObject) angelElement).get(byName));
+    }
+
+    private static String initializeParams(JsonElement element) {
+        return element == null || element instanceof JsonNull ? "" : element.getAsString();
+    }
 }
